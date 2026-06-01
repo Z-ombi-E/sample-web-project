92ecb96d42192f80eedd3f5b4ac09376c96b8f16
Z-ombi-E <nethumnenula2004@gmail.com>
2026-06-01 13:56:18 +0530

    Basic Implementations

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 # Welcome!
+## SAMPLE WEB PROJECT
---
+++ script.js
@@ -0,0 +1,5 @@
+const message = document.getElementById("message");
+
+function showMessage(){
+    message.textContent = "CI/CD Pipeline Working!"
+}
