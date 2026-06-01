1720bda80f690883631662ed8b3831a9c7c9b063
Z-ombi-E <nethumnenula2004@gmail.com>
2026-06-01 14:15:10 +0530

    Additional Button -clear

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const message = document.getElementById("message");
 
 function showMessage(){
-    message.textContent = "CI/CD Pipeline Working!"
+    message.textContent = "CI/CD Pipeline Working!";
+    console.log("qqq");
+}
+
+function clearMessage(){
+    message.textContent = " ";
+    console.log("qqqqqqq");
 }
